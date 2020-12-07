644bbded633066091ec1e30df1584c59a1e4daf2
Edson Alcala <edson_alcala@msn.com>
2020-12-07 07:34:45 +0000

    moved styles and added commented analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from "react"
 import { Routes } from "./routes"
 import { RemixProvider } from "./hooks"
 
+import "./App.css";
+
 const App = () => {
   return (
     <RemixProvider>
---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export enum constants {
+  NEW_CONTRACT = 'NEW_CONTRACT',
+}
+
+export const PUSH1 = 0x60
+
+export const PUSH2 = 0x61
+
+export const PUSH32 = 0x7f
+
+export const TIMESTAMP = 0x42
